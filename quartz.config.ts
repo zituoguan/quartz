3054714fd4194c25d5aa4cf3a0e5862bff52afaa
r3f <9460939+r3f@users.noreply.github.com>
2025-05-28 17:36:54 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Quartz 4 文档",
-    pageTitleSuffix: "Quartz",
+    pageTitleSuffix: " - Quartz",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
